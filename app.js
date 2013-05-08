61b0ef65f34595b84d66f220befff0208ccb16d9
leslie li <leslie.li@live.com>
2013-05-08 22:22:54 +0800

    add repo to git

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var express = require('express')
   , user_request = require('./routes/user_request')
   , http = require('http')
   , path = require('path')
-  //, parseCookie = require('connect').utils.parseCookie
   , cookieStore = require('connect/lib/middleware/session/memory')
   , flash = require('connect-flash');
+
 var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -21,14 +21,25 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.cookieParser());
+
+// express session
 app.use(express.session({
     secret:"leslie&sophia",
     store: new cookieStore
 }));
 app.use(express.methodOverride());
+
+// app.dynamicHelpers
+app.use(function(req, res, next){
+    res.locals.csrf = req.session ? req.session._csrf : '';
+    res.locals.req = req;
+    res.locals.session = req.session;
+    next();
+});
 app.use(app.router);
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -40,10 +51,13 @@ app.get('/login', routes.login);
 app.get('/register', routes.register);
 app.get('/home', routes.home);
 
+
 // user_account part
 app.post('/register', user_account.register);
 app.post('/login', user_account.login);
+app.get('/logout', user_account.logout);
 app.get('/find', user_account.find);
+
 
 // user request part
 app.post('/requestFriend', user_request.requestFriend);
