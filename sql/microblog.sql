0cf26b2d13669dfa61cc68c8866ddc00b0e5d10a
RockBoyLeslie <leslie.li@live.com>
2013-05-08 11:06:14 +0800

    add repo to git

---
+++ sql/microblog.sql
@@ -0,0 +1,175 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: microblog
+-- ------------------------------------------------------
+-- Server version	5.0.87-community-nt
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL auto_increment,
+  `content` varchar(200) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message_id` int(11) NOT NULL,
+  `status` varchar(11) NOT NULL default 'approved',
+  `agent` varchar(10) NOT NULL default 'website',
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dynamic_messages`
+--
+
+DROP TABLE IF EXISTS `dynamic_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dynamic_messages` (
+  `id` int(11) NOT NULL auto_increment,
+  `content` varchar(200) NOT NULL,
+  `style` varchar(10) NOT NULL default 'friend',
+  `user_id` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL default 'published',
+  `year` varchar(4) NOT NULL,
+  `month` varchar(2) NOT NULL,
+  `day` varchar(2) NOT NULL,
+  `comments` int(11) default '0',
+  `agent` varchar(10) default 'website',
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dynamic_messages`
+--
+
+LOCK TABLES `dynamic_messages` WRITE;
+/*!40000 ALTER TABLE `dynamic_messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dynamic_messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `private_messages`
+--
+
+DROP TABLE IF EXISTS `private_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `private_messages` (
+  `id` int(11) NOT NULL auto_increment,
+  `from_user` int(11) NOT NULL,
+  `to_user` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `parent_id` int(11) default '0',
+  `is_read` varchar(1) NOT NULL default '0',
+  `status` varchar(1) NOT NULL default '0',
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `private_messages`
+--
+
+LOCK TABLES `private_messages` WRITE;
+/*!40000 ALTER TABLE `private_messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `private_messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_accounts`
+--
+
+DROP TABLE IF EXISTS `user_accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_accounts` (
+  `id` int(11) NOT NULL auto_increment,
+  `email` varchar(60) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `totle_friends` int(11) default '0',
+  `totle_messages` int(11) default '0',
+  `error_login` int(11) default '0',
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_accounts`
+--
+
+LOCK TABLES `user_accounts` WRITE;
+/*!40000 ALTER TABLE `user_accounts` DISABLE KEYS */;
+INSERT INTO `user_accounts` VALUES (1,'leslie.li@live.com','leslie','leslie',0,0,0,'2013-05-07 03:41:11'),(2,'lichao8858749@hotmail.com','12','12',0,0,0,'2013-05-07 13:33:28');
+/*!40000 ALTER TABLE `user_accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_relationships`
+--
+
+DROP TABLE IF EXISTS `user_relationships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_relationships` (
+  `id` int(11) NOT NULL auto_increment,
+  `inviter` int(11) NOT NULL,
+  `invitee` int(11) NOT NULL,
+  `type` varchar(15) NOT NULL default 'friend',
+  `status` varchar(10) NOT NULL default 'pending',
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_relationships`
+--
+
+LOCK TABLES `user_relationships` WRITE;
+/*!40000 ALTER TABLE `user_relationships` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_relationships` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-05-07 22:24:24
