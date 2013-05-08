c6fdfdd2e6931c8ca6fe5e7defbcd90ab33f5799
leslie li <leslie.li@live.com>
2013-05-08 22:42:20 +0800

    add repo to git

--- routes/user_account.js
+++ routes/user_account.js
@@ -14,25 +14,30 @@ exports.register = function(req, res) {
     }
 
     pool.getConnection(function(err, connection) {
-        connection.query('select 1 from microblog.user_accounts where email = ?', [email], function(err, rows, fields){
-            if (err) {
-                throw err;
-            }
-            if(rows[0]){
-                data.response_message = '邮箱已经存在',
-                res.render('register',data);
-            }
+        try{
+            connection.query('select 1 from microblog.user_accounts where email = ?', [email], function(err, rows, fields){
+                if (err) {
+                    throw err;
+                }
+                if(rows[0]){
+                    data.response_message = '邮箱已经存在',
+                    res.render('register',data);
+                }
 
-            var values = [email, username, password];
-            connection.query( 'insert into  microblog.user_accounts(email, name, password) values(?, ?, ?)', values, function(err, rows) {
-                 if (err) {
-                     throw err;
-                 }else {
-                     res.send('register succeed');
-                 }
+                var values = [email, username, password];
+                connection.query( 'insert into  microblog.user_accounts(email, name, password) values(?, ?, ?)', values, function(err, rows) {
+                     if (err) {
+                         throw err;
+                     }else {
+                         res.send('register succeed');
+                     }
+                });
             });
+        } catch (err) {
+            res.send(err);
+        } finally {
             connection.end();
-        });
+        }
     });
 };
 
@@ -41,15 +46,21 @@ exports.login =  function(req, res) {
     var password = req.body['password'];
     var values = [email, password];
     pool.getConnection(function(err, connection){
-        connection.query('select email, id , name from microblog.user_accounts where email = ? and password = ?', values, function(err,rows,fields){
+        try{
+            connection.query('select email, id , name from microblog.user_accounts where email = ? and password = ?', values, function(err,rows,fields){
+
+                if(!rows[0]){
+                    res.render('login', {title:'登录', response_message:'邮箱或密码错误', email:email});
+                } else {
+                    req.session.user = rows[0];
+                    res.redirect('/home');
+                }
+            })
+        } catch (err) {
+            res.send(err);
+        } finally {
             connection.end();
-            if(!rows[0]){
-                res.render('login', {title:'登录', response_message:'邮箱或密码错误', email:email});
-            } else {
-                req.session.user = rows[0];
-                res.redirect('/home');
-            }
-        })
+        }
     })
 };
 
@@ -57,21 +68,27 @@ exports.find = function(req, res) {
     var keyword = '%' + req.query.keyword + '%';
     var current_user = req.session.user.id;
     pool.getConnection(function(err, connection) {
-        connection.query(
-            'select u.*, r.type from (select id , name, email from microblog.user_accounts  where (email like ? or name like ?) and id != ?) u ' +
-                'left join (select friend_id, type from microblog.user_relationships where user_id = ?) r on u.id = r.friend_id;',
-            [keyword, keyword, current_user, current_user],
-            function(err, rows, fields) {
-                if (err) {
-                    throw err;
+        try {
+            connection.query(
+                'select u.*, r.type from (select id , name, email from microblog.user_accounts  where (email like ? or name like ?) and id != ?) u ' +
+                    'left join (select friend_id, type from microblog.user_relationships where user_id = ?) r on u.id = r.friend_id;',
+                [keyword, keyword, current_user, current_user],
+                function(err, rows, fields) {
+                    if (err) {
+                        throw err;
+                    }
+                    var data = {
+                        title : '搜索结果',
+                        users : rows
+                    };
+                    res.render('users/find', data);
                 }
-                var data = {
-                    title : '搜索结果',
-                    users : rows
-                };
-                res.render('users/find', data);
-            }
-        )
+            )
+        } catch (err) {
+            res.send(err);
+        } finally {
+            connection.end();
+        }
     });
 };
 
--- routes/user_request.js
+++ routes/user_request.js
@@ -4,28 +4,34 @@ exports.requestFriend = function(req, res) {
     var invitee = req.body['invitee'];
     var current_user = req.session.user.id;
     pool.getConnection(function(err, connection) {
-        connection.query(
-            "select status from microblog.user_requests where inviter = ? and invitee = ? and type = 'friend'", [current_user, invitee],function(err, rows, fields) {
-                if (err) {
-                    throw err;
-                }
-                if (rows[0]) {
-                    var user_request =  rows[0];
-                    if (user_request.status == 'approved') {
-                        res.json({response_code : '0', response_message : '你俩已经是好友了'});
-                    }
-                    if (user_request.status == 'pending') {
-                        res.json({response_code : '0', response_message : '好友请求发送成功'});
-                    }
-                }
-
-                connection.query('insert into microblog.user_requests(inviter, invitee) values (?,?)', [current_user, invitee], function(err, rows) {
+        try{
+            connection.query(
+                "select status from microblog.user_requests where inviter = ? and invitee = ? and type = 'friend'", [current_user, invitee],function(err, rows, fields) {
                     if (err) {
                         throw err;
                     }
-                    res.json({response_code : '0', response_message : '好友请求发送成功'});
-                });
-            }
-        )
+                    if (rows[0]) {
+                        var user_request =  rows[0];
+                        if (user_request.status == 'approved') {
+                            res.json({response_code : '0', response_message : '你俩已经是好友了'});
+                        }
+                        if (user_request.status == 'pending') {
+                            res.json({response_code : '0', response_message : '好友请求发送成功'});
+                        }
+                    }
+
+                    connection.query('insert into microblog.user_requests(inviter, invitee) values (?,?)', [current_user, invitee], function(err, rows) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.json({response_code : '0', response_message : '好友请求发送成功'});
+                    });
+                }
+            )
+        } catch(err) {
+            res.send(err);
+        } finally {
+            connection.end();
+        }
     });
 };
