f9a06cf1bd28f4428ab14cfb6b32912053afb9c6
RockBoyLeslie <leslie.li@live.com>
2013-05-10 14:54:52 +0800

    move requestFriend to friend.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,22 +19,4 @@ $(function(){
 
         }
     });
-
-    //请求添加好友
-    $("button[name='requestFriend']").click(function(){
-        var id = $(this).attr("for");
-        $.ajax({
-            url : "/requestFriend",
-            type : "post",
-            data : "invitee=" + id,
-            dataType : 'json',
-            success : function(data) {
-                if (data.response_code == 0) {
-                    alert(data.response_message);
-                } else {
-                    alert("好友请求发送失败， 请稍后再试");
-                }
-            }
-        });
-    });
 });
